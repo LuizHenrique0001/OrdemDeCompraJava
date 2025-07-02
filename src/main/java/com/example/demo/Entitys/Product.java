59acd8fc9ac9289ae427489ffd003db500040f08
Luizhenrique0001 <rodriguesluizhenrique316@gmail.com>
2025-07-02 14:24:44 -0300

    Product criado e suas categorias

---
+++ src/main/java/com/example/demo/Entitys/Product.java
@@ -0,0 +1,79 @@
+package com.example.demo.Entitys;
+
+import com.example.demo.Enum.Category;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "Product")
+public class Product {
+
+    @Id
+    private String id;
+    private String name;
+    private String description;
+    private Double price;
+    private String imgUrl;
+
+    private Category categories;
+
+    public Product() {
+    }
+
+    public Product(String id, String name, String description, Double price, String imgUrl, Category categories) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.imgUrl = imgUrl;
+        this.categories = categories;
+    }
+
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    public Category getCategories() {
+        return categories;
+    }
+
+    public void setCategories(Category categories) {
+        this.categories = categories;
+    }
+}
