5904c1fa11893ec78e9ae90361bd68cb1b0387be
Luizhenrique0001 <rodriguesluizhenrique316@gmail.com>
2025-07-04 10:30:32 -0300

    ProductDto criado

---
+++ src/main/java/com/example/demo/DTO/ProductDto.java
@@ -0,0 +1,40 @@
+package com.example.demo.DTO;
+
+import com.example.demo.Entitys.Product;
+
+public class ProductDto {
+
+    private String idProduct;
+    private String nameProduct;
+    private Double priceProduct;
+
+    public ProductDto(Product product){
+        this.idProduct = product.getId();
+        this.nameProduct = getNameProduct();
+        this.priceProduct = getPriceProduct();
+    }
+
+    public String getIdProduct() {
+        return idProduct;
+    }
+
+    public void setIdProduct(String idProduct) {
+        this.idProduct = idProduct;
+    }
+
+    public String getNameProduct() {
+        return nameProduct;
+    }
+
+    public void setNameProduct(String nameProduct) {
+        this.nameProduct = nameProduct;
+    }
+
+    public Double getPriceProduct() {
+        return priceProduct;
+    }
+
+    public void setPriceProduct(Double priceProduct) {
+        this.priceProduct = priceProduct;
+    }
+}
