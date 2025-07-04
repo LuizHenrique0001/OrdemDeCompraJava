29c2374fd166d1dc5c0d88d264e2ff5463dd6f95
Luizhenrique0001 <rodriguesluizhenrique316@gmail.com>
2025-07-04 10:29:44 -0300

    Ajuste OrderStatus

--- src/main/java/com/example/demo/Enum/OrderStatus.java
+++ src/main/java/com/example/demo/Enum/OrderStatus.java
@@ -2,11 +2,31 @@ package com.example.demo.Enum;
 
 public enum OrderStatus {
 
-    AGUARDANDO_AUTORIZACAO_A1,
-    AGUARDANDO_AUTORIZACAO_A2,
-    AGUARDANDO_AUTORIZACAO_A3,
-    AUTORIZADO_AGUARDANDO_COMPRA,
-    COMPRADO_AGUARDANDO_RETIRADA,
-    ORDEM_FINALIZADA
+    AGUARDANDO_AUTORIZACAO_A1(1),
+    AGUARDANDO_AUTORIZACAO_A2(2),
+    AGUARDANDO_AUTORIZACAO_A3(3),
+    AUTORIZADO_AGUARDANDO_COMPRA(4),
+    COMPRADO_AGUARDANDO_RETIRADA(5),
+    ORDEM_FINALIZADA(6);
+
+    private int code;
+
+    private OrderStatus(Integer code) {
+        this.code = code;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public static OrderStatus velueOf(int code) {
+        for (OrderStatus orderStatus : OrderStatus.values()) {
+            if (orderStatus.code == code) {
+                return orderStatus;
+            }
+
+        }
+        throw new IllegalArgumentException("Invalid Status code");
+    }
 
 }
